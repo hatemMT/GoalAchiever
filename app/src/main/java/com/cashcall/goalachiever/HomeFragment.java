685821b5dffb11463826e1e6d90c3e5a1f97b645
Hatem <hatem.hm.7@gmail.com>
2020-12-22 08:51:43 +0200

    Adding navigation base

---
+++ app/src/main/java/com/cashcall/goalachiever/HomeFragment.java
@@ -0,0 +1,32 @@
+package com.example.goalachiever;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewDataBinding binding = DataBindingUtil.inflate(inflater,
+                R.layout.fragment_home,
+                container,
+                false);
+
+        return binding.getRoot();
+    }
+}
